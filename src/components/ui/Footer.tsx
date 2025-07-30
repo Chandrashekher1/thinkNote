71cec56cd1bdfb273be14c9d9676d92d7671220d
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-31 01:44:09 +0530

    feat: add footer component

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,16 @@
+import { GithubIcon, LinkedinIcon, XIcon } from "lucide-react";
+
+export function Footer() {
+  return (
+    <footer className="bg-default text-white py-8">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">© {new Date().getFullYear()} <span className="font-bold">Chandrashekher</span>. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-4">
+            <a href="https://github.com/Chandrashekher1"><GithubIcon/></a>
+            <a href="www.linkedin.com/in/chandrashekher-prasad-a496a2293"><LinkedinIcon/></a>
+            <a href="https://x.com/cpsaw03" className="b"><XIcon/></a>
+        </div>
+      </div>
+    </footer>
+  );
+}
