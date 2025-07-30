b0a3fc364280753db090a6d00930222047f944cf
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-30 15:03:50 +0530

    feat: add shadcn card

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,40 +1,92 @@
-import { ShareIcon } from "@/icons/ShareIcon";
+import * as React from "react"
 
+import { cn } from "@/lib/utils"
 
-interface CardProps{
-    title: string
-    link: string
-    type: "twitter" | "youtube" 
+function Card({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card"
+      className={cn(
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
-export function Card({title, link, type}: CardProps) {
-    return (
-        <div className="my-8 flex flex-wrap">
-            <div className="bg-white rounded-md border border-slate-100 shadow-md outline-slate-200 p-4 w-[20vw] h-[40vh] overflow-y-scroll">
-                <div className="flex justify-between sticky top-0 z-10">
-                    <div className="text-gray-500 flex items-center">
-                        <ShareIcon size="sm"/>
-                        <span className="text-black mx-2">{title}</span>
-                    </div>
-                    <div className="flex">
-                        <div className="pr-2 text-gray-500">
-                            <a href={link} target="_blank">
-                                <ShareIcon size="sm"/>
-                            </a>
-                        </div>
-                        <div className="text-gray-500">
-                            <ShareIcon size="sm"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="pt-4">
-                { type=== "youtube" && <iframe  src={link.replace("watch","embed").replace("?v=","/")} className="w-full rounded-md" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
-                {type === "twitter" && <blockquote className="twitter-tweet">
-                    <a href={link.replace("x.com","twitter.com")}></a> 
-                </blockquote>}
-                </div>
-            </div>
-        </div>
-    )
-        
+function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-header"
+      className={cn(
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-title"
+      className={cn("leading-none font-semibold", className)}
+      {...props}
+    />
+  )
+}
+
+function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-description"
+      className={cn("text-muted-foreground text-sm", className)}
+      {...props}
+    />
+  )
+}
+
+function CardAction({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-action"
+      className={cn(
+        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-content"
+      className={cn("px-6", className)}
+      {...props}
+    />
+  )
+}
+
+function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-footer"
+      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+      {...props}
+    />
+  )
+}
+
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
 }
