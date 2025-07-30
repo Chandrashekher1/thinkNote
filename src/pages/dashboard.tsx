7b17a71322d30151de84178b5d7785816130c744
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-31 02:31:21 +0530

    style: adjust margin and padding for responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -253,7 +253,7 @@
           ) : (
             <motion.div variants={fadeInUp} initial="hidden" whileInView="visible" viewport={{ once: true }} className='mx-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-8'>
               {fetchContent.map(({ link, title, type, tags , _id}, index) => (
-                <Card key={index} className="bg-white dark:bg-primary-foreground border border-border rounded-2xl shadow-md transition-colors hover:-translate-y-1 hover:duration-300">
+                <Card key={index} className="md:w-[30vw] md:h-[30vh] w-[90vw] bg-white dark:bg-primary-foreground border border-border rounded-2xl shadow-md transition-colors hover:-translate-y-1 hover:duration-300">
                   <CardHeader className="flex items-start justify-between gap-2">
                     <div className="flex items-center gap-2 text-xl font-semibold text-foreground">
                       {type === 'youtube' && <YoutubeIcons />} {type === 'twitter' && <TwitterIcon />} {type === 'document' && <DockIcon />} {title}
