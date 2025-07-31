abf79ea0bf5b5c2aaf475574e0318ef831d9deab
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-01 01:18:24 +0530

    fix: remove unused import path

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { cn } from "../../lib/utils";
-import { BadgeX, CircleXIcon, FolderClosedIcon, X } from "lucide-react";
+import { CircleXIcon } from "lucide-react";
 
 interface PopoverContextType {
   open: boolean;
