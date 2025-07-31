925cf176b1b7c3462e7425cd51cd2f352890332f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-01 01:46:06 +0530

    fix: fix path

--- src/components/ui/signup-form.tsx
+++ src/components/ui/signup-form.tsx
@@ -1,12 +1,13 @@
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/Button"
 import { Card, CardContent } from "@/components/ui/Card"
-import { Input } from "../ui/input"
+// import { Input } from "../ui/input"
 import { Label } from "../ui/label"
 import { easeOut, motion } from "motion/react"
 import { useNavigate } from "react-router-dom"
 import { useRef } from "react"
 import { BACKEND_URL } from "@/config"
+import { Input } from "./input"
 
 export function SignForm({
   className,
