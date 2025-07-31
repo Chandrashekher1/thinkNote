137dddc2fdfcc653179db4e1f64f85253965a7b5
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-31 23:54:44 +0530

    feat: add path of shareLink

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import Dashboard from "./pages/dashboard";
 import { Home } from "./pages/Home";
 import LoginPage from "./pages/login";
 import { Footer } from "./components/ui/Footer";
+import UserDashboard from "./pages/userDashboard";
 
 function App(){
   return(
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/brain/:shareLink" element={<UserDashboard/>}/>
         </Routes> 
         <Footer />
       </BrowserRouter>
