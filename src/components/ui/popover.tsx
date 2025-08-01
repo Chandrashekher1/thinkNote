06434f790b2c8540cfa211c2bc976392e67fdd8f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-01 14:55:56 +0530

    fix: fix popvercontext

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -192,4 +192,4 @@ const PopoverContent = React.forwardRef<HTMLDivElement, PopoverContentProps>(
 );
 PopoverContent.displayName = "PopoverContent";
 
-export { Popover, PopoverTrigger, PopoverContent };
+export { Popover, PopoverTrigger, PopoverContent ,PopoverContext };
