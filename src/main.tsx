4cfe7ddd5e33aff033ff32b0b884dd0f3bb32002
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-02 17:39:43 +0530

    fix: remove import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from './components/ui/theme-provider.tsx'
 import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
